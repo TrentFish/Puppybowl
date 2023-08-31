1b06e5a2035e27be890c62c6bde99a2be0d217d1
fishyboi@MSI <fishyboi@MSI>
2023-08-31 14:21:56 -0400

    The players appear in HTML

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+const puppy = document.querySelector(".puppy");
+console.log(puppy);
 
 async function getPlayers(){
     const info = await fetch(" https://fsa-puppy-bowl.herokuapp.com/api/2109-UNF-HY-WEB-PT/players");
     const jason = await info.json();
     const players = jason.data;
+
     console.log(players);
+    const html = players.players.map(player => {
+        return `
+        <div>
+            <a href="#${player.id}">${player.name}</a>
+            <p>${player.breed}</p>
+        </div>
+        `;
+    }).join("");
+    puppy.innerHTML = html;
 }
 
 getPlayers();
