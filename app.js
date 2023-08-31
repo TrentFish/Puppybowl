d4922f82cf5b2025da5cfd0053dffd55f3713747
fishyboi@MSI <fishyboi@MSI>
2023-08-31 13:46:50 -0400

    Playing Fetch

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("Bark Bark");
+
+async function getPlayers(){
+    const info = await fetch(" https://fsa-puppy-bowl.herokuapp.com/api/2109-UNF-HY-WEB-PT/players");
+    const jason = await info.json();
+    const players = jason.data;
+    console.log(players);
+}
+
+getPlayers();
