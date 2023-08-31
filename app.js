d4f6498aa75c6ab3bbb7c64e6622e09099857097
fishyboi@MSI <fishyboi@MSI>
2023-08-31 17:50:40 -0400

    Render

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 const puppy = document.querySelector(".puppy");
 console.log(puppy);
 
+let players;
+
 async function getPlayers(){
     const info = await fetch(" https://fsa-puppy-bowl.herokuapp.com/api/2109-UNF-HY-WEB-PT/players");
     const jason = await info.json();
-    const players = jason.data;
+    players = jason.data;
+    render();
+}
 
-    console.log(players);
+function render(){
+    const hash = window.location.hash.slice(1)*1;
     const html = players.players.map(player => {
         return `
         <div>
-            <a href="#${player.id}">${player.name}</a>
-            <p>${player.breed}</p>
+            <a href="#${player.id}" class="${player.id === hash ? "selected": ""}">${player.name}</a>
         </div>
         `;
     }).join("");
     puppy.innerHTML = html;
 }
+
+window.addEventListener("hashchange", () =>{
+    render();
+});
 
 getPlayers();
